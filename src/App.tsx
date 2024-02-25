10f000a2bd0b7a56052f828296cfdecb08065864
deekshithmd <deekshithmogra@gmail.com>
2024-02-25 12:43:43 +0530

    added husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
 import AuthPage from "./pages/AuthPage";
 import FirstPage from "./pages/FirstPage";
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
 import SecondPage from "./pages/SecondPage";
-import { bfunction1,afunction1,cfunction1 } from './services/api.service';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -32,11 +30,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  useEffect(()=>{
-afunction1();
-bfunction1();
-cfunction1();
-  },[])
+  console.log("hello");
   return (
     <div>
       <RouterProvider router={router} />
