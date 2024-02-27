8c18227f4e25ddd4abfba656c731df5234a93224
deekshithmd <deekshithmogra@gmail.com>
2024-02-27 21:36:44 +0530

    added oauth

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-mqbz1bshhmt0vqlx.us.auth0.com"
+      clientId="tzpdOPGyodZiPoS4frHVQ7HqdxFWp0x8"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 );
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,11 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link, Outlet } from "react-router-dom";
 export default function LandingPage() {
+  const { loginWithRedirect, user, logout, isAuthenticated } = useAuth0();
+  console.log("connected", user);
   return (
     <div>
       <Link to="/auth" className="underline text-blue-700">
         Auth
       </Link>
       <h1 className="text-2xl text-red-500">Landing Page edit</h1>
+      {!isAuthenticated ? (
+        <button className="border" onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      ) : (
+        <div>
+          <img src={user?.picture} className="h-10 w-10 rounded-full" />
+          <button
+            className="border"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+        </div>
+      )}
       <Outlet />
     </div>
   );
